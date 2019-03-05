2b4edf57e8d503c62cda82c2d839a3b9c06dd847
ChenMingMing <belovedyogurt@gmail.com>
2019-03-05 17:55:19 +0800

    relation

--- lib/orm.dart
+++ lib/orm.dart
@@ -36,6 +36,9 @@ abstract class Model {
       if (symbol == cls.simpleName) {
         return;
       }
+      if (decl.metadata.isEmpty) {
+        return;
+      }
       attrsMap[getSymbolName(symbol)] = decl.metadata.single.getField(#type).reflectee;
     });
     return attrsMap;
@@ -53,9 +56,7 @@ abstract class Model {
     });
     print(keys);
     var fields = keys.join(',');
-    var values = keys.map((k){
-      return "\'${data.getField(Symbol(k)).reflectee}\'";
-    }).join(',');
+    var values = getValues(keys.toList(), data);
     var sql = """
       insert into app_${getSymbolName(cls.simpleName)} ($fields)
         values ($values) returning "id", ${keys.map((k) => "\"$k\"").join(',')}
@@ -64,15 +65,13 @@ abstract class Model {
     var results = await connection.query(sql);
     syncDataToModel(attrsMap.keys.toList(), results.first, reflect(this));
   }
-  
+
   update() async {
     var data = reflect(this);
     ClassMirror cls = data.type;
     var attrsMap = getAttrsMap(cls);
     var keys = attrsMap.keys.skipWhile((k) => k == 'id');
-    var values = keys.map((k){
-      return "$k=\'${data.getField(Symbol(k)).reflectee}\'";
-    }).join(',');
+    var values = getValues(keys.toList(), data);
     var sql = """
       update app_${getSymbolName(cls.simpleName)} set $values where id=${data.getField(#id).reflectee}
     """;
@@ -94,6 +93,7 @@ abstract class Model {
     var keys = attrsMap.keys.toList();
     var fieldsString = keys.join(',');
     var sql = "select $fieldsString from app_${getSymbolName(cls.simpleName)} where id=$id";
+    print(sql);
     var list = await connection.query(sql);
     if (list.isEmpty) {
       return null;
@@ -102,6 +102,9 @@ abstract class Model {
     if (data.isNotEmpty) {
       var model = cls.newInstance(Symbol(''), []);
       syncDataToModel(keys, data, model);
+      if (model.type.declarations.keys.contains(#prepare)) {
+        await model.invoke(#prepare, [model.reflectee]).reflectee;
+      }
       return model.reflectee as T;
     }
     return null;
@@ -111,6 +114,24 @@ abstract class Model {
 class Type{
   final String type;
   const Type(this.type);
+}
+
+class Book extends Model {
+  @Type('serial primary key')
+  int id;
+
+  @Type('varchar(255)')
+  String name;
+
+  @Type('int')
+  int author_id;
+
+  User author;
+
+  Future<Book> prepare(Book book) async {
+    book.author = await Model.findById<User>(book.author_id);
+    return book;
+  }
 }
 
 class User extends Model {
@@ -131,20 +152,45 @@ syncDataToModel(List keys, List datas, InstanceMirror model) {
     }
 }
 
+String getValues(List keys, InstanceMirror data){
+  return keys.map((k){
+      var v = data.getField(Symbol(k)).reflectee;
+      if (v == null) {
+        return 'null';
+      }
+      return "\'$v\'";
+    }).join(',');
+}
+
 
 main(List<String> args) async {
   await init();
+  // await Model.createTable<Book>();
   // await Model.createTable<User>();
+  // var book = await Model.findById<Book>(1);
+  // print(book.id);
+  // print(book.author_id);
+  // print(book.author);
+  var author = User();
+  author.username = 'hello';
+  await author.save();
+  print(author.id);
+  var b = Book();
+  b.name = "20";
+  b.author_id = author.id;
+  await b.save();
+  var b2 = await Model.findById<Book>(b.id);
+  print(b2.author.id);
   // var u = User();
   // u.username = 'hello';
   // var ret = u.save();
-  var user = User();
-  user.username = 'h1';
-  await user.save();
-  print(user.username);
-  user.username = "h2";
-  await user.update();
-  print(user.username);
+  // var user = User();
+  // user.username = 'h1';
+  // await user.save();
+  // print(user.username);
+  // user.username = "h2";
+  // await user.update();
+  // print(user.username);
 
   // print(user);
   // print(user.username);
